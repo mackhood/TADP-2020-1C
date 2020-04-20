a8c00c569fa8e87ef0d528b95efadd4ab3567784
gerjugo <germanjugo@hotmail.com>
2020-04-19 23:34:30 -0300

    primer modelado de traits

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -1,6 +1,23 @@
-class Prueba
+require "byebug"
 
-  def materia
-    :tadp
+class Trait
+  def self.define(&a_block)
+    new_trait = Trait.new()
+
+    new_trait.instance_eval(&a_block)
+  end
+
+  def method(symbol, &block)
+    self.define_singleton_method(symbol) do
+      block
+    end
+  end
+
+  def name(symbol)
+    objeto = self
+
+    the_symbol = symbol
+
+    Object.const_set(the_symbol, objeto)
   end
 end
