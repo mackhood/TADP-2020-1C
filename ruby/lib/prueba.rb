bb56bbddc2877cd1ea60e1344dd4804df1c5bc25
gerjugo <germanjugo@hotmail.com>
2020-04-21 16:12:27 -0300

    avance con el diseño de trait problema con metodo2

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -2,22 +2,33 @@ require "byebug"
 
 class Trait
   def self.define(&a_block)
-    new_trait = Trait.new()
+    byebug
+    new_trait = Trait.new
 
     new_trait.instance_eval(&a_block)
   end
 
   def method(symbol, &block)
-    self.define_singleton_method(symbol) do
+    byebug
+    define_singleton_method(symbol) do
       block
     end
   end
 
   def name(symbol)
     objeto = self
-
     the_symbol = symbol
-
     Object.const_set(the_symbol, objeto)
+  end
+end
+
+class Class
+  def uses(object)
+    byebug
+    object.singleton_methods.each do |method|
+      byebug
+      a_proc = method.to_proc
+      self.define_method(method.to_s, &a_proc) unless self.instance_methods(false).include? method
+    end
   end
 end
