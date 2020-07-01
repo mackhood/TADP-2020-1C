6933f9a4533f0dcb57cf8410c4de5222582fa924
gerjugo <germanjugo@hotmail.com>
2020-06-30 22:32:53 -0300

    modificaciones torneo

--- scala/src/main/scala/torneo/Torneo.scala
+++ scala/src/main/scala/torneo/Torneo.scala
@@ -1,11 +1,11 @@
 package torneo
 
-import Participante.Participante
 import dragon.Dragon
 import posta.Posta
-
 import scala.util.Success
-
+import Participante.Participante
+import _root_.Participante.jinete.Jinete
+import _root_.Participante.vikingo.Vikingo
 
 case class Torneo(
                    participantes: Array[Participante],
@@ -13,8 +13,9 @@ case class Torneo(
                    dragonesDisponibles: Array[Dragon] = Array()
                  ) {
 
-  def resultadoTorneo: Array[Participante] = postas.foldLeft(participantes)((participantesRestantes: Array[Participante], unaPosta: Posta) => {
-    if(participantesRestantes.length == 1) {
+  def resultadoTorneo: Array[Participante] = {postas.foldLeft(participantes)
+    ((participantesRestantes: Array[Participante], unaPosta: Posta) => {
+    if(participantesRestantes.length <= 1) {
       // TODO: Se ejecutan postas sin participantes
       participantesRestantes
     }else{
@@ -22,4 +23,26 @@ case class Torneo(
     }
 
   })
+  }
+
+
+  def prepararParticipantes(participantes: Array[Participante],dragonesDisponibles: Array[Dragon],posta: Posta):Array[Participantes] ={
+    var participantesGenerados:  Array[Participantes] = Array()
+    var listaDragonesAuxiliares: ArrayBuffer[Dragon] = dragonesDisponibles.clone().toBuffer
+
+    //val participante2 = Vikingo(50, 200, 5, 70)
+
+
+    participantes.foreach { participante =>
+       val variable = participante.match {
+         //
+       }
+    }
+
+
+
+
+
+
+  }
 }
