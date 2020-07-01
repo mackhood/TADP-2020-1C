230e834f9a6f86d4f45428c4603519a884b9e5d1
gerjugo <germanjugo@hotmail.com>
2020-06-30 22:44:34 -0300

    comento lo q no funciona

--- scala/src/main/scala/torneo/Torneo.scala
+++ scala/src/main/scala/torneo/Torneo.scala
@@ -2,10 +2,13 @@ package torneo
 
 import dragon.Dragon
 import posta.Posta
+
 import scala.util.Success
 import Participante.Participante
 import _root_.Participante.jinete.Jinete
 import _root_.Participante.vikingo.Vikingo
+
+import scala.collection.mutable.ArrayBuffer
 
 case class Torneo(
                    participantes: Array[Participante],
@@ -13,36 +16,35 @@ case class Torneo(
                    dragonesDisponibles: Array[Dragon] = Array()
                  ) {
 
-  def resultadoTorneo: Array[Participante] = {postas.foldLeft(participantes)
-    ((participantesRestantes: Array[Participante], unaPosta: Posta) => {
-    if(participantesRestantes.length <= 1) {
-      // TODO: Se ejecutan postas sin participantes
-      participantesRestantes
-    }else{
-      unaPosta.podioPosta(participantesRestantes)
-    }
-
-  })
-  }
-
-
-  def prepararParticipantes(participantes: Array[Participante],dragonesDisponibles: Array[Dragon],posta: Posta):Array[Participantes] ={
-    var participantesGenerados:  Array[Participantes] = Array()
-    var listaDragonesAuxiliares: ArrayBuffer[Dragon] = dragonesDisponibles.clone().toBuffer
-
-    //val participante2 = Vikingo(50, 200, 5, 70)
+//  def resultadoTorneo: Array[Participante] = {postas.foldLeft(participantes)
+//    ((participantesRestantes: Array[Participante], unaPosta: Posta) => {
+//    if(participantesRestantes.length <= 1) {
+//      // TODO: Se ejecutan postas sin participantes
+//      participantesRestantes
+//    }else{
+//      unaPosta.podioPosta(participantesRestantes)
+//    }
+//
+//  })
+//  }
 
 
-    participantes.foreach { participante =>
-       val variable = participante.match {
-         //
-       }
-    }
-
+//  def prepararParticipantes(participantes: Array[Participante],dragonesDisponibles: Array[Dragon],posta: Posta):Array[Participante] ={
+//    var participantesGenerados:  Array[Participante] = Array()
+//    var listaDragonesAuxiliares: ArrayBuffer[Dragon] = dragonesDisponibles.clone()
+//
+//    val participante2 = Vikingo(50, 200, 5, 70)
+//
+////    participantes.foreach { participante =>
+////       val variable = participante.match {
+////         //
+////       }
+////    }
 
 
 
 
 
-  }
+
+
 }
