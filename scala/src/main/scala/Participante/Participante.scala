0820e86105408dde22817564d494d649559bd674
gerjugo <germanjugo@hotmail.com>
2020-07-03 19:13:45 -0300

    corrijo nuevo issue otro refactor y agrego modificaciones de Delegar en el participante #14

--- scala/src/main/scala/Participante/Participante.scala
+++ scala/src/main/scala/Participante/Participante.scala
@@ -4,4 +4,6 @@ import posta.Posta
 
 trait Participante {
   def esMejorQue(elOtroParticipante: Participante)(posta: Posta): Boolean = posta.resultadoParticipante(this) >= posta.resultadoParticipante(elOtroParticipante)
+  def aumentarHambre(nivelHambre: Int)
+
 }
