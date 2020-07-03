b1af19d47ffe0775c6e2dd8d521282b0b846546c
gerjugo <germanjugo@hotmail.com>
2020-07-03 15:47:20 -0300

    nuevos avances

--- scala/src/main/scala/torneo/Regla/Regla.scala
+++ scala/src/main/scala/torneo/Regla/Regla.scala
@@ -2,9 +2,10 @@ package torneo.Regla
 
 import Participante.Participante
 import dragon.Dragon
+import posta.Posta
 
 abstract  class Regla {
 
-  def previoPosta(participantes: Array[Participante],dragones :Array[Dragon]): Array[Participante]
+  def previoPosta(participantes: Array[Participante],dragones :Array[Dragon], posta: Posta): Array[Participante]
   def postPosta(participantes: Array[Participante]): Array[Participante]
 }
--- scala/src/main/scala/torneo/Torneo.scala
+++ scala/src/main/scala/torneo/Torneo.scala
@@ -7,47 +7,28 @@ import scala.util.Success
 import Participante.Participante
 import _root_.Participante.jinete.Jinete
 import _root_.Participante.vikingo.Vikingo
+import torneo.Regla.{Estandar, Regla}
 
 import scala.collection.mutable.ArrayBuffer
 
 case class Torneo(
                    participantes: Array[Participante],
                    postas: Array[Posta],
-                   dragonesDisponibles: Array[Dragon] = Array()
+                   dragonesDisponibles: Array[Dragon] = Array(),
+                   regla: Regla= Estandar()
                  ) {
 
   def resultadoTorneo: Array[Participante] = {postas.foldLeft(participantes)((participantesRestantes: Array[Participante], unaPosta: Posta) => {
-      if (participantesRestantes.length == 1) {
+
+    if (participantesRestantes.length == 1) {
         // TODO: Se ejecutan postas sin participantes
         participantesRestantes
       } else {
-        unaPosta.podioPosta(prepararParticipantes(participantesRestantes, dragonesDisponibles, unaPosta))
-      }
+
+        regla.postPosta(unaPosta.podioPosta(regla.previoPosta(participantesRestantes, dragonesDisponibles, unaPosta)))
+    }
     })
   }
-
-
-
-
- def prepararParticipantes(participantes: Array[Participante],dragonesDisponibles: Array[Dragon],posta: Posta):Array[Participante] ={
-    var participantesGenerados:  Array[Participante] = Array()
-    var listaDragonesAuxiliares: Array[Dragon] = dragonesDisponibles.clone()
-
-    participantes.foreach { x =>
-      x match {
-        case a: Vikingo => participantesGenerados.+:(a.mejorMontura(listaDragonesAuxiliares,posta))
-        case _ => println("Caso todavia no contemplado")
-      }
-
-      //listaDragonesAuxiliares.filterNot(dragon_ => dragon_.estaDisponible(participantesGenerados))
-    }
-   participantesGenerados
-    }
-
-
-
-
-
 
 
 }
